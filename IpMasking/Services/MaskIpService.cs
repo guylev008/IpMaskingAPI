fe694022c58e8023c5e570bd342fe28df9664ae3
guy <guy.selenium@gmail.com>
2020-12-06 17:46:47 +0200

    refactor

--- IpMasking/Services/MaskIpService.cs
+++ IpMasking/Services/MaskIpService.cs
@@ -23,19 +23,10 @@ namespace IpMasking.Services
         public MemoryStream HandleMasking(IFormFile i_file)
         {
             var fileContent = i_file.ReadAsStringAsync();
-            var ipList = GetClassCIpsFromFileContent(fileContent);
-            foreach (var ip in ipList)
-            {
-                var maskedIp = _maskIps.Mask(ip);
-                fileContent = fileContent.Replace(ip, maskedIp);
-            }
-
-            return fileContent.ConvertToStream();
+            var content = _maskIps.Mask(fileContent);
+            return content.ConvertToStream();
         }
 
-        private IEnumerable<string> GetClassCIpsFromFileContent(string fileContent)
-        {
-            return Regex.Matches(fileContent, @"(19[2-9]|2[0-2]\d|23[0-3])(\.([1-9]?\d|[12]\d\d)){3}").Select(m => m.Groups[0].Value).Distinct();
-        }
+       
     }
 }
--- MaskIp/MaskIps.cs
+++ MaskIp/MaskIps.cs
@@ -1,24 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace MaskIp
 {
     public interface IMaskIps
     {
-        string Mask(string i_ip);
+        string Mask(string i_content);
     }
     public class MaskIps : IMaskIps
     {
-        private string _networkAddress;
         private readonly Lazy<Random> _rand = new Lazy<Random>();
-
-        public string Mask(string i_ip)
+        public string Mask(string i_content)
         {
-            if (string.IsNullOrEmpty(_networkAddress))
+            var ipsList = GetIpsFromFileContent(i_content);
+            var mask = _rand.Value.Next(1, 256);
+            foreach (var ip in ipsList)
             {
-                _networkAddress = $"{_rand.Value.Next(1, 256)}.{_rand.Value.Next(1, 256)}.{_rand.Value.Next(1, 256)}";
+                var ipAddress = IPAddress.Parse(ip);
+                var maskedIp = MaskIpAddress(ipAddress, mask);
+                i_content = i_content.Replace(ip, maskedIp);
             }
 
-            return $"{_networkAddress}.{_rand.Value.Next(1, 256)}";
+            return i_content;
+        }
+
+        private string MaskIpAddress(IPAddress ip, int mask)
+        {
+            var ipAddressBytes = ip.GetAddressBytes();
+            var maskedIpAddressValues = ipAddressBytes.Select(ipAddressByte => ipAddressByte ^ mask);
+            return string.Join('.', maskedIpAddressValues);
+        }
+
+        private IEnumerable<string> GetIpsFromFileContent(string fileContent)
+        {
+            return Regex.Matches(fileContent, @"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")
+                .Select(m => m.Value)
+                .Distinct();
         }
     }
 }
